c79e96e30d82b9242ba6d5ba0257265e0e02eafb
Yuval Dekel <thedekel@gmail.com>
2012-02-25 01:16:28 -0500

    aenuthoecuhoeaurch

--- content.js
+++ content.js
@@ -1,13 +1,11 @@
 chrome.extension.onRequest.addListener(
     function(req, sender, sendback){
-        console.log("anything");
-        console.log(req);
-        sendback();
         if (req.command == "startemode"){
             console.log('starting emode');
         }else if (req.command == "endmode"){
             console.log('stop emode');
         }
+        sendback();
     }
 );
 
