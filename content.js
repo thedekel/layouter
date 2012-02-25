e8ed628d13d977becc0ac515e6c4fd85221181e9
Yuval Dekel <thedekel@gmail.com>
2012-02-25 07:40:36 -0500

    recuharcuhaoeurroe ercuhaoeuroetn joarchoerihoea

--- content.js
+++ content.js
@@ -5,37 +5,86 @@ var setsize = function(obj){
     rules[obj.id]["height"] = obj.he;
     console.log(rules);
 }
+var setpos = function(obj){
+    rules[obj.id] = rules[obj.id] || new Object();
+    rules[obj.id]["x"] = obj.x;
+    rules[obj.id]["y"] = obj.y;
+    console.log(rules);
+}
 
 var movediv = function(el){
-	window.onclick = function(ev){
-		console.log("clicked, x,y ="+ev.x + " " + ev.y);
-		el.style.position = "absolute";
-		el.style.left = ev.x;
-		el.style.top = ev.y;
-	}
-}
-
-var resizediv = function(el){
-    window.onclick = function(ev){
-        console.log("clicked, x,y = "+ev.x + " " + ev.y);
-        var t = el.offsetTop;
-        var l = el.offsetLeft;
-        console.log("l,t = "+l + " " + t);
-        if (ev.x>= l && ev.y >= t){
-            console.log("here");
-            el.style.width = ev.x-l+"px";
-            el.style.height = ev.y-t+"px";
-        }else {
-            return;
+    window.onkeydown = function(ev){
+        if (ev.keyCode == 27){
+            console.log("I'm here");
+            var newstyle = document.createElement("style");
+            newstyle.innerHTML = "div:hover{border:2px solid blue}";
+            document.head.appendChild(newstyle);
+            window.onkeydown = function(ev){
+                if (ev.keyCode == 27){
+                    console.log("escape");
+                    document.head.removeChild(newstyle);
+                    window.onclick = null;
+                    window.onkeydown = null;          
+                }
+            }
+            window.onclick = function(ev){
+                var el = ev.target;
+                while (el.tagName != "DIV"){
+                    el = el.parentNode;
+                    if (el.tagName == "BODY"){
+                        console.log('body reached, stopping')
+                        return;
+                    }
+                }
+                try{
+                document.head.removeChild(newstyle);
+                }catch(Exception){}
+             //   el.style.border = "2px solid blue";
+                curElement = el;
+                movediv(el);
+            }
         }
-        var ret = {
-            "id": el.id,
-            "wi": el.width,
-            "he": el.height
-        }
-        setsize(ret);
     }
-}
+	window.onclick = function(ev){
+        if (ev.ctrlKey){
+            console.log("clicked, x,y = "+ev.x + " " + ev.y);
+            var t = el.offsetTop;
+            var l = el.offsetLeft;
+            console.log("l,t = "+l + " " + t);
+            if (ev.x>= l && ev.y >= t){
+                console.log("here");
+                document.body.appendChild(el);
+                el.style['-webkit-transition'] = 'all 1s ease-out';
+                el.style.width = ev.x-l+"px";
+                el.style.height = ev.y-t+"px";
+                el.style.overflow = "scroll";
+            }else {
+                return;
+            }
+            var ret = {
+                "id": el.id,
+                "wi": el.width,
+                "he": el.height
+            }
+            setsize(ret);
+        }else{
+            console.log("clicked, x,y ="+ev.x + " " + ev.y);
+            document.body.appendChild(el);
+            el.style['-webkit-transition'] = 'all 1s ease-out';
+            el.style.position = "absolute";
+            el.style.left = ev.x +"px";
+            el.style.top = ev.y + "px";
+            var ret = {
+                "id": el.id,
+                "x" : ev.x,
+                "y" : ev.y
+            }
+            setpos(ret);
+        }
+    }
+}   
+
+    
 swap = function(id1, id2) {
 	el1 = document.getElementById(id1);
 	el2 = document.getElementById(id2);
@@ -74,10 +123,17 @@ chrome.extension.onRequest.addListener(
         if (req.command == "startemode"){
             console.log('38');
             console.log('starting emode');
-            //set it up so divs do stuff on hover
             var newstyle = document.createElement("style");
-            newstyle.innerHTML = "div:hover{border:2px solid blue}\ndiv{border:1px solid red}";
+            newstyle.innerHTML = "div:hover{border:2px solid blue}";
             document.head.appendChild(newstyle);
+            window.onkeydown = function(ev){
+                if (ev.keyCode == 27){
+                    console.log("escape");
+                    document.head.removeChild(newstyle);
+                    window.onclick = null;
+                    window.onkeydown = null;          
+                }
+            }
             window.onclick = function(ev){
                 var el = ev.target;
                 while (el.tagName != "DIV"){
@@ -90,10 +146,9 @@ chrome.extension.onRequest.addListener(
                 try{
                 document.head.removeChild(newstyle);
                 }catch(Exception){}
-                el.style.border = "2px solid blue";
+                //el.style.border = "2px solid blue";
                 curElement = el;
-                resizediv(el);
-
+                movediv(el);
             }
 
         }else if (req.command == "endmode"){
