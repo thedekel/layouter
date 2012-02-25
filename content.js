98b9977f8d05ba64b222b4b938223d475aea8d1a
Yuval Dekel <thedekel@gmail.com>
2012-02-25 16:15:57 -0500

    demo time

--- content.js
+++ content.js
@@ -1,15 +1,104 @@
 var rules = {};
+chrome.extension.sendRequest({"command":"fetch", "url":location.href},function(response){
+    console.log(response);
+    if (!response.rules){
+        return;
+    }
+    rules = response.rules;
+    for (i in rules){
+        //get element
+        var el;
+        if (typeof(i)=='string' && i.charAt(0)=="#"){
+            el = document.getElementById(i.substr(1));
+        }else if (typeof(i)=='string' && i.charAt(0)=="."){
+            var q = parseInt(i.substr(1));
+            var qq = i.substr(1+q.toString().length);
+            el = document.getElementsByClassName(qq)[q];
+        }else{
+            el = document.getElementsByTagName("DIV")[parseInt(i)];
+        }
+        //x and y
+        if (rules[i].hasOwnProperty("x")){
+            document.body.appendChild(el);
+            el.style.position="absolute";
+            el.style.left = rules[i].x+"px";
+            el.style.top = rules[i].y+"px";
+        }
+        //width and height
+        if (rules[i].hasOwnProperty("width")){
+                el.style.width = rules[i].width;
+                el.style.height = rules[i].height;
+                el.style.overflow = "scroll";
+                el.style.minHeight="inherit";
+                el.style.minWidth="inherit";
+        }
+        //display
+        if (rules[i].hasOwnProperty("display")){
+            el.style.display = "none";
+        }
+    }
+});
 var setsize = function(obj){
-    rules[obj.id] = rules[obj.id] || new Object();
-    rules[obj.id]["width"] = obj.wi;
-    rules[obj.id]["height"] = obj.he;
+    var oid;
+    if (obj.id){
+        oid = "#"+obj.id;
+    }else if (obj.class){
+        var clist = document.getElementsByClassName(obj.class);
+        for (i = 0;i<clist.length; i++){
+            if (obj == clist[i]){
+                oid = "."+i+obj.class;
+            }
+        }
+    }else{
+        var dlist = document.getElementsByTagName("DIV");
+        for (i = 0;i<dlist.length; i++){
+            if (obj == dlist[i]){
+                oid = i+obj;
+            }
+        }
+    }
+    console.log("60, oid"+oid);
+    if (!rules.hasOwnProperty(oid)){
+        rules[oid] = {};
+    }
+    rules[oid]["width"] = obj.wi;
+    rules[oid]["height"] = obj.he;
     console.log(rules);
+                    if (rules){
+                        console.log(rules);
+                        chrome.extension.sendRequest({"loc":location.href, "str":JSON.stringify(rules)});
+                    }
 }
 var setpos = function(obj){
-    rules[obj.id] = rules[obj.id] || new Object();
-    rules[obj.id]["x"] = obj.x;
-    rules[obj.id]["y"] = obj.y;
+     var oid;
+    if (obj.id){
+        oid = "#"+obj.id;
+    }else if (obj.class){
+        var clist = document.getElementsByClassName(obj.class);
+        for (i = 0;i<clist.length; i++){
+            if (obj == clist[i]){
+                oid = "."+i+obj.class;
+            }
+        }
+    }else{
+        var dlist = document.getElementsByTagName("DIV");
+        for (i = 0;i<dlist.length; i++){
+            if (obj == dlist[i]){
+                oid = i+obj;
+            }
+        }
+    }
+    console.log("85, oid="+oid);
+    if (!rules.hasOwnProperty(oid)){
+        rules[oid] = {};
+    }
+    rules[oid]["x"] = obj.x;
+    rules[oid]["y"] = obj.y;
     console.log(rules);
+                    if (rules){
+                        console.log(rules);
+                        chrome.extension.sendRequest({"loc":location.href, "str":JSON.stringify(rules)});
+                    }
 }
 
 var movediv = function(el){
@@ -25,7 +114,11 @@ var movediv = function(el){
                     document.head.removeChild(newstyle);
                     window.onclick = null;
                     window.onkeydown = null;          
-                }
+                    if (rules){
+                        console.log(rules);
+                        chrome.extension.sendRequest({"loc":location.href, "str":JSON.stringify(rules)});
+                    }
+                }            
             }
             window.onclick = function(ev){
                 var el = ev.target;
@@ -39,11 +132,39 @@ var movediv = function(el){
                 try{
                 document.head.removeChild(newstyle);
                 }catch(Exception){}
-             //   el.style.border = "2px solid blue";
+                el.style.border = "2px solid blue";
                 curElement = el;
                 movediv(el);
             }
+        } else if (ev.keyCode == 68){
+                    console.log("am I here???");
+                    el.style.display = "none";
+
+                
+ var oid;
+    if (el.id){
+        oid = "#"+el.id;
+    }else if (el.class){
+        var clist = document.getElementsByClassName(el.class);
+        for (i = 0;i<clist.length; i++){
+            if (el == clist[i]){
+                oid = "."+i+el.class;
+            }
         }
+    }else{
+        var dlist = document.getElementsByTagName("DIV");
+        for (i = 0;i<dlist.length; i++){
+            if (el == dlist[i]){
+                oid = i+el;
+            }
+        }
+    }
+    if (!rules.hasOwnProperty(oid)){
+        rules[oid] = {};
+    }
+    rules[oid]["display"] = "none";
+    console.log(rules);
+}
     }
 	window.onclick = function(ev){
         if (ev.ctrlKey){
@@ -58,13 +179,15 @@ var movediv = function(el){
                 el.style.width = ev.x-l+"px";
                 el.style.height = ev.y-t+"px";
                 el.style.overflow = "scroll";
+                el.style.minHeight="inherit";
+                el.style.minWidth="inherit";
             }else {
                 return;
             }
             var ret = {
                 "id": el.id,
-                "wi": el.width,
-                "he": el.height
+                "wi": el.style.width,
+                "he": el.style.height
             }
             setsize(ret);
         }else{
@@ -132,6 +255,10 @@ chrome.extension.onRequest.addListener(
                     document.head.removeChild(newstyle);
                     window.onclick = null;
                     window.onkeydown = null;          
+                    if (rules){
+                        console.log(rules);
+                        chrome.extension.sendRequest({"loc":location.href, "str":JSON.stringify(rules)});
+                    }
                 }
             }
             window.onclick = function(ev){
@@ -146,7 +273,7 @@ chrome.extension.onRequest.addListener(
                 try{
                 document.head.removeChild(newstyle);
                 }catch(Exception){}
-                //el.style.border = "2px solid blue";
+                el.style.border = "2px solid blue";
                 curElement = el;
                 movediv(el);
             }
